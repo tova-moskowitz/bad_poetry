466ed4060a05f19cc1c69c98261429a07abb0ca8
Tova Moskowitz <tovamosk@gmail.com>
2016-11-13 15:02:03 -0500

    cleanup indentation, extract example component into separate file

---
+++ src/components/App/statelessComponentExample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from '../HelloWorld';
+
+// ecma6 syntax, and a stateless component
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
