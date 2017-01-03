70df48106de12cd65a1172021a14883032cff20c
Devorah Moskowitz <devorah.moskowitz@nytimes.com>
2017-01-02 19:07:10 -0500

    fix config

--- kyt.config.js
+++ kyt.config.js
@@ -1,4 +1,10 @@
 
 module.exports = {
-  reactHotLoader: true,
+    reactHotLoader: true,
+    modifyWebpackConfig: (baseConfig) => {
+        // Fixes issue preventing importing of fs node module
+        // https://github.com/pugjs/pug-loader/issues/8#issuecomment-55568520
+        baseConfig.node.fs = 'empty';
+        return baseConfig;
+    }
 };
