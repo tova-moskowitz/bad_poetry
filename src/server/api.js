702b110a78720ccb59be7770a77016e15847c9fa
Devorah <devorahrm@gmail.com>
2017-02-05 14:24:41 -0500

    refactor random word getters and api endpoints to GET api/random-words/ and GET api/random-words/{partOfSpeech}; Add support for transform options via query param

--- src/server/api.js
+++ src/server/api.js
@@ -1,44 +1,32 @@
 import { Router } from 'express';
-import wordBank, { getRandomWord } from '../wordBank';
+import wordBank from '../wordBank';
 
 const router = Router();
 
-router.get('/random-word/:partOfSpeech', (req, res) => {
-    const partOfSpeech = req.params.partOfSpeech;
-    const arr = wordBank[partOfSpeech];
+router.get('/random-words', (req, res) => {
+    const results = {};
 
-    if (!arr) {
+    Object.keys(wordBank).forEach((part) => {
+        results[part] = wordBank[part]();
+    });
+
+    res.json(results);
+});
+
+router.get('/random-words/:partOfSpeech', (req, res) => {
+    const part = req.params.partOfSpeech;
+    const wordGetterFn = wordBank[part];
+
+    if (typeof wordGetterFn !== 'function') {
         res.status(404).json({
-            error: `${partOfSpeech} is an invalid part of speech`
+            error: `${part} is an invalid part of speech`
         });
         return;
     }
 
     res.json({
-        [partOfSpeech]: getRandomWord(arr)
+        [part]: wordGetterFn(req.query)
     });
-});
-
-router.post('/random-words', (req, res) => {
-    const words = req.body && req.body.words;
-    const results = {};
-
-    if (!words) {
-        res.status(404).json({
-            error: 'No words specified'
-        });
-        return;
-    }
-
-    words.map((pos) => {
-        const arr = wordBank[pos];
-        if (arr) {
-            results[pos] = getRandomWord(arr);
-        }
-        return true;
-    });
-
-    res.json(results);
 });
 
 export default router;
--- src/wordBank/index.js
+++ src/wordBank/index.js
@@ -1,54 +1,53 @@
-import adjective from './parts/adjective';
-import adverb from './parts/adverb';
-import conjunction from './parts/conjunction';
-import interjection from './parts/interjection';
-import nounObject from './parts/nounObject';
-import nounSubject from './parts/nounSubject';
-import possessive from './parts/possessive';
-import preposition from './parts/preposition';
-import pronoun from './parts/pronoun';
-import verb from './parts/verb';
-import noun from './parts/noun';
-import { day, month, unitOfTime, season } from './parts/time';
-import { capitalizeFirstLetter } from './utils';
+import partsOfSpeech from './partsOfSpeech';
+import * as utils from './utils';
 
-export default {
-    adjective,
-    adverb,
-    conjunction,
-    interjection,
-    noun,
-    nounObject,
-    nounSubject,
-    possessive,
-    preposition,
-    pronoun,
-    verb,
-    day,
-    month,
-    unitOfTime,
-    season
-};
+/**
+ * Handle transformation options for a word as specified in the endpoint query.
+ For example, a user may request a word to be capitalized or pluralized.
+ They may pass ?key=true or just ?key. Either one will trigger the transform.
 
-export function getRandomWord(wordArray, opts) {
+ GET /api/random-words/noun/?pluralize&withIndefiniteArticle
+ GET /api/random-words/noun/?capitalizeFirstLetter=true
+
+ * @param {object} transformOpts The transform options requested for this word
+    e.g. { pluralize: true } or { withIndefiniteArticle: '' }
+ * @param {string} word The word to be transformed
+ * @returns {string} Returns the original word with any appropriate
+ transformations applied
+ */
+function handleTransformOpts(transformOpts, word) {
+    let transformed = word;
+
+    Object.keys(transformOpts).forEach((opt) => {
+        const transformFn = utils[opt];
+
+        if (typeof transformFn === 'function') {
+            transformed = transformFn(word);
+        }
+    });
+
+    return transformed;
+}
+
+function getRandomWord(wordArray, transformOpts) {
     const index = Math.floor(Math.random() * wordArray.length);
-    const word = wordArray[index];
+    let word = wordArray[index];
 
-    opts = opts || {};
+    transformOpts = transformOpts || {};
 
-    if (opts.capitalizeFirstLetter) {
-        return capitalizeFirstLetter(word);
-    }
+    // Apply any requested transformations
+    word = handleTransformOpts(transformOpts, word);
 
     return word;
 }
 
-export function generateWordFetchers(allWordArrays) {
+function generateWordFetchers() {
     const ret = {};
-    Object.keys(allWordArrays).map((part) => {
-        ret[part] = opts => getRandomWord(allWordArrays[part], opts);
-        return null;
+    Object.keys(partsOfSpeech).forEach((part) => {
+        ret[part] = opts => getRandomWord(partsOfSpeech[part], opts);
     });
 
     return ret;
 }
+
+export default generateWordFetchers();
