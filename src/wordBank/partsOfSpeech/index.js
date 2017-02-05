cecc3f213437c7f1b1ee5db6f748b04cd96b31b7
Devorah <devorahrm@gmail.com>
2017-02-05 14:23:29 -0500

    rename wordBank/parts => wordBank/partsOfSpeech; add index file

---
+++ src/wordBank/partsOfSpeech/index.js
@@ -0,0 +1,30 @@
+import adjective from './adjective';
+import adverb from './adverb';
+import conjunction from './conjunction';
+import interjection from './interjection';
+import nounObject from './nounObject';
+import nounSubject from './nounSubject';
+import possessive from './possessive';
+import preposition from './preposition';
+import pronoun from './pronoun';
+import verb from './verb';
+import noun from './noun';
+import { day, month, unitOfTime, season } from './time';
+
+export default {
+    adjective,
+    adverb,
+    conjunction,
+    interjection,
+    noun,
+    nounObject,
+    nounSubject,
+    possessive,
+    preposition,
+    pronoun,
+    verb,
+    day,
+    month,
+    unitOfTime,
+    season
+};
